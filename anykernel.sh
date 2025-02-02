737b0821e436ef5b5776040e5aad5707b355c922
romiyusnandar <yusromi04@gmail.com>
2025-02-02 08:31:31 +0000

    Changes for sweet
    Signed-off-by: romiyusnandar <yusromi04@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -1,10 +1,10 @@
-# AnyKernel3 Ramdisk Mod Script
-# osm0sis @ xda-developers
+### AnyKernel3 Ramdisk Mod Script
+## osm0sis @ xda-developers
 
-## AnyKernel setup
-# begin properties
+### AnyKernel setup
+# global properties
 properties() { '
-kernel.string=
+kernel.string=Kernel by romiz
 do.devicecheck=1
 do.modules=0
 do.systemless=1
@@ -12,38 +12,22 @@ do.cleanup=1
 do.cleanuponabort=0
 device.name1=sweet
 device.name2=sweetin
-device.name3=
-supported.versions=11 - 14.1
+supported.versions=11 - 15
+supported.patchlevels=
+supported.vendorpatchlevels=
 '; } # end properties
 
-boot_attributes() {
-set_perm_recursive 0 0 755 644 $ramdisk/*;
-set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
-} # end attributes
 
 # boot shell variables
-block=/dev/block/bootdevice/by-name/boot;
-is_slot_device=0;
-ramdisk_compression=auto;
-patch_vbmeta_flag=auto;
+BLOCK=/dev/block/bootdevice/by-name/boot;
+IS_SLOT_DEVICE=0;
+RAMDISK_COMPRESSION=auto;
 
-
-## AnyKernel methods (DO NOT CHANGE)
-# import patching functions/variables - see for reference
+# import functions/variables and setup patching - see for reference (DO NOT REMOVE)
 . tools/ak3-core.sh;
 
-## Selinux Permissive
-#patch_cmdline androidboot.selinux androidboot.selinux=permissive
-
-# migrate from /overlay to /overlay.d to enable SAR Magisk
-
-# end ramdisk changes
 # boot install
-dump_boot; # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
+dump_boot;
 
-write_boot; 
-## end install
-
-sleep 3
-ui_print ""
-ui_print "kernel installed successfully"
+write_boot;
+## end boot install
